a71d2e989c16b78698879c28534ddd03682d6d58
Hacksterbanda <154428655+Hacksterbanda@users.noreply.github.com>
2026-05-31 21:40:37 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@
 // ═══════════════════════════════════════════════════════════
 const firebaseConfig = {
   apiKey:            "AIzaSyCbNa8LYblS0DNWFghfFqiMdc5r_exrsKo",
-  authDomain:        "newforadt.firebase.com",
+  authDomain:        "newforadt.firebaseapp.com",
   projectId:         "newforadt",
   storageBucket:     "newforadt.firebasestorage.app",
   messagingSenderId: "269144820825",
