8f85540d56eb2da9ae6d2a38f4abea97d7d894f2
Hacksterbanda <154428655+Hacksterbanda@users.noreply.github.com>
2026-05-31 18:07:20 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@
 // ═══════════════════════════════════════════════════════════
 const firebaseConfig = {
   apiKey:            "AIzaSyCbNa8LYblS0DNWFghfFqiMdc5r_exrsKo",
-  authDomain:        "adt-groupsltd.vercel.app",
+  authDomain:        "newforadt.firebase.com",
   projectId:         "newforadt",
   storageBucket:     "newforadt.firebasestorage.app",
   messagingSenderId: "269144820825",
